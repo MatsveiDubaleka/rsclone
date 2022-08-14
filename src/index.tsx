f834baba74b329ee9cf6d0b4ce80e8d250eb5a78
Pavel <pavel.priladyshev@gmail.com>
2022-08-14 11:54:29 +0300

    feat: add header

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header/Header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <>
+    <Header />
+  </>
+);
 
 reportWebVitals();
