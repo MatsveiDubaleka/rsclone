c445bdd9a9955b98cabd335f2a88a8d101ddf557
Pavel <pavel.priladyshev@gmail.com>
2022-08-14 21:57:44 +0300

    refactor: moved header to App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/Header/Header';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <>
-    <Header />
-  </>
+    <App />
 );
 
 reportWebVitals();
