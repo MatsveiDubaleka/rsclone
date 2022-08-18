9a0fb27cfcd9fefc1c45873c904497948b500fc9
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-18 20:06:24 +0300

    feat: add link to MongoDB

--- server/db/config.js
+++ server/db/config.js
@@ -1,41 +1,18 @@
-/*import mongoose from "mongoose";
+import mongoose from "mongoose";
 import express from "express";
-import exphbs from "express-handlebars";
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
-const hbs = exphbs.create({
-    defaultLayout: "main",
-    extname: 'hbs'
-})
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
-
-async function start() {
+export async function start() {
     try{
-        await mongoose.connect('mongodb+srv://MatsveiDubaleka:64251matvey@cluster0.zwuj6px.mongodb.net/customreviews', {
-            useNewUrlParser: true,
-            useFindAndModify: false
+        await mongoose.connect('mongodb+srv://MatsveiDubaleka:m29092003O@cluster0.zwuj6px.mongodb.net/customreviews',
+        )
+        app.listen(PORT, (hostname) => {
+            console.log('Server has been started...', hostname)
         })
-        app.listen(PORT, () => {
-            console.log('Server has been started...')
-        })
-    } catch(e) {
-        throw(e);
+    } catch(error) {
+        console.error(error);
     }
 }
-
-start().then(r => console.log(r));
- */
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://MatsveiDubaleka:<password>@cluster0.zwuj6px.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
