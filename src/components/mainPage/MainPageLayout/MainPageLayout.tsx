60769dfbb4d0f63e35fe8b417a5f012e351fd4b8
Vera Krasnova <azirafail@mail.ru>
2022-08-18 22:54:05 +0300

    RSClone-2-main-page-layout

---
+++ src/components/mainPage/MainPageLayout/MainPageLayout.tsx
@@ -0,0 +1,12 @@
+import MovieCard from "../MovieCard/MovieCard"
+import "./MainPageLayout.scss"
+
+export const MainPageLayout = () => {
+	return(
+		<main className="main-page">
+			<div className="side-menu"></div>
+			<div className="main-page__content">
+			</div>
+		</main>
+	)
+}
