b6654f7302f3600f889825fdc427d6dfbbca4c13
Vera Krasnova <azirafail@mail.ru>
2022-08-18 23:02:54 +0300

    RSClone-2-main-page-layout

--- src/components/mainPage/MainPageLayout/MainPageLayout.tsx
+++ src/components/mainPage/MainPageLayout/MainPageLayout.tsx
@@ -1,11 +1,17 @@
+import { MainPageMenu } from "../MainPageMenu/MainPageMenu"
 import MovieCard from "../MovieCard/MovieCard"
+import MovieCardsList from "../MovieCardsList/MovieCardsList"
+import { YouInterestedList } from "../YouInterestedList/YouInterestedList"
 import "./MainPageLayout.scss"
 
 export const MainPageLayout = () => {
 	return(
 		<main className="main-page">
-			<div className="side-menu"></div>
+			<MainPageMenu></MainPageMenu>
 			<div className="main-page__content">
+				<MovieCardsList title="Рекомендации"></MovieCardsList>
+				<MovieCardsList title="Новинки"></MovieCardsList>
+				<YouInterestedList></YouInterestedList>
 			</div>
 		</main>
 	)
