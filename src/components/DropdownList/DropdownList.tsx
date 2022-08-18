687475c0d78046ac7c4e2810b3dc917de040821f
Vera Krasnova <74105045+VerchkaFeya@users.noreply.github.com>
2022-08-18 22:57:37 +0300

    RS-Clone 22 dropdown search list (#9)

---
+++ src/components/DropdownList/DropdownList.tsx
@@ -0,0 +1,46 @@
+import "./DropdownList.scss";
+import { IDropdownMovieList, IDropdownMovie } from "./types";
+
+const DropdownMovieItem = ({nameRu, nameEn, posterUrlPreview, year, rating, filmId} : IDropdownMovie) => {
+
+	let ratingClass = 'dropdown-movie-item__rating';
+
+	if (Number(rating) < 5) {
+		ratingClass += ' negative';
+	} else if (Number(rating) < 7) {
+		ratingClass += ' neutral';
+	} else {
+		ratingClass += ' positive';
+	}
+
+	return(
+		<div className="dropdown-movie-item" data-id={filmId}>
+			<div className="dropdown-movie-item__poster" style={{backgroundImage: `url(${posterUrlPreview})`}}></div>
+			<div className="dropdown-movie-item__info">
+				<p className="dropdown-movie-item__title">{nameRu}</p>
+				<p className={ratingClass}>{rating},</p>
+				<p className="dropdown-movie-item__title-en">{nameEn},</p>
+				<p className="dropdown-movie-item__year">{year}</p>
+			</div>
+		</div>
+	)
+}
+
+const DropdownList = ({movies} : IDropdownMovieList) => {
+
+	movies.splice(5);
+
+	return (
+		<div className="dropdown-list">
+			<div className="dropdown-list__movies">
+				{movies.map((movie: IDropdownMovie) : JSX.Element => {
+					return <DropdownMovieItem  nameRu={movie.nameRu} nameEn={movie.nameEn} year={movie.year} rating={movie.rating} posterUrlPreview={movie.posterUrlPreview} key={`dropdown-movie-item-${movie.filmId}`}></DropdownMovieItem>
+				})}
+			</div>
+			<button className="dropdown-list__show-btn">Показать всё</button>
+		</div>
+	)
+}
+
+export default DropdownList;
+
---
+++ src/components/DropdownList/types.ts
@@ -0,0 +1,13 @@
+export interface IDropdownMovie {
+	nameRu: string,
+	nameEn: string,
+	posterUrlPreview: string,
+	year: number,
+	rating: number,
+	filmId?: number
+}
+
+
+export interface IDropdownMovieList {
+	movies: IDropdownMovie[]
+}
