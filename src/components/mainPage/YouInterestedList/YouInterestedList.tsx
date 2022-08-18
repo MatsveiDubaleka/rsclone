4ad1375475ae4e2f1fba3c075d6840ee7deac86b
Vera Krasnova <azirafail@mail.ru>
2022-08-18 23:15:53 +0300

    RSClone-21-you-interested-list

---
+++ src/components/mainPage/YouInterestedList/YouInterestedList.tsx
@@ -0,0 +1,17 @@
+import "./YouInterestedList.scss";
+
+export const YouInterestedList = () => {
+	return(
+		<div className="you-interested-list">
+		<h2 className="you-interested-list__title">Вы интересовались</h2>
+		<div className="you-interested-list__movies">
+			<div className="you-interested-list__movie"></div>
+			<div className="you-interested-list__movie"></div>
+			<div className="you-interested-list__movie"></div>
+			<div className="you-interested-list__movie"></div>
+			<div className="you-interested-list__movie"></div>
+			<div className="you-interested-list__movie"></div>
+		</div>
+	</div>
+	)
+}
