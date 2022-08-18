1714d936881425f25414316f7002309a32bb5644
Pavel <pavel.priladyshev@gmail.com>
2022-08-18 17:08:09 +0300

    feat: add Authorization page layout

---
+++ src/components/Authorization/Authorization.tsx
@@ -0,0 +1,22 @@
+import './Authorization.scss';
+
+const Authorization = () => {
+  return (
+    <div className="authorization-wrapper">
+      <div className="authorization">
+        <div className="authorization-card">
+          <div className="authorization-card__logo-img"></div>
+          <span className="authorization-card__tooltip">Войдите или зарегестрируйтесь</span>
+          <form action="">
+            <input name="login" type="text" placeholder="Логин и email" className="authorization-card__input login-auth" id="login-auth"/>
+            <input name="password" type="text" placeholder="Пароль" className="authorization-card__input password-auth" id="password-auth"/>
+          </form>
+          <button className="authorization-card__button-auth"></button>
+          <button className="authorization-card__button-auth"></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Authorization;
