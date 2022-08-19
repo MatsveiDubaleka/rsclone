bd71de2ee89f2f9ef2500946644b192f4357badc
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-19 13:55:53 +0300

    feat: add authRouter

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import {start} from "./server/db/config.js";
+const mongoose = require("mongoose")
+const express = require("express")
+const autoRouter = require('./authRouter')
+const PORT = process.env.PORT || 8000
 
-start().then(r => console.log(r));
+const app = express()
+
+app.use(express.json())
+app.use('./auth', autoRouter)
+
+async function start() {
+  try{
+    await mongoose.connect('mongodb+srv://MatsveiDubaleka:m29092003O@cluster0.zwuj6px.mongodb.net/auth_roles')
+    app.listen(PORT, () => {
+      console.log(` Server has been started ${PORT}`)
+    })
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+start()
