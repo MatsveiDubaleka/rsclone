867810529256a8a4b29749d639dedece72463818
Vera Krasnova <azirafail@mail.ru>
2022-08-19 15:48:23 +0300

    RSClone-28: layout for interesting facts

--- src/components/moviePage/InterestingFacts/InterestingFacts.tsx
+++ src/components/moviePage/InterestingFacts/InterestingFacts.tsx
@@ -2,8 +2,17 @@ import "./InterestingFacts.scss";
 
 export const InterestingFacts = () => {
 	return(
-		<div>
-			
+		<div className="interesting-facts">
+		<h3 className="interesting-facts__title">Знаете ли вы, что...</h3>
+		<div className="interesting-facts__content">
+			<span className="interesting-facts__marker"></span>
+			<p className="interesting-facts__text">Несколько месяцев назад в глухой деревушке бесследно пропал ребенок. 
+				Спустя время все почти забыли о происшедшем. Однажды в местную школу приезжает молодая учительница. 
+				Вместе с маленьким сыном она пытается начать новую жизнь, подальше сбежав от своего прошлого. 
+				Цепь загадочных событий указывает на то, что появление мальчика могло вновь пробудить древнее зло, 
+				затаившееся в окрестных лесах.
+			</p>
 		</div>
+	</div>
 	)
 }
