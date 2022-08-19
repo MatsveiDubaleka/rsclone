dddfbdd0cfd68102a0bb8bc2a1bb4ab6c9c02304
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-19 15:24:00 +0300

    feat: init DB

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const mongoose = require("mongoose")
 const express = require("express")
 const autoRouter = require('./authRouter')
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
-app.use('./auth', autoRouter)
+app.use('/auth', autoRouter)
 
-async function start() {
+const start = async () => {
   try{
-    await mongoose.connect('mongodb+srv://MatsveiDubaleka:m29092003O@cluster0.zwuj6px.mongodb.net/auth_roles')
-    app.listen(PORT, () => {
-      console.log(` Server has been started ${PORT}`)
-    })
-  } catch(error) {
-    console.error(error);
+    await mongoose.connect('mongodb+srv://MatsveiDubaleka:m29092003O@cluster0.zwuj6px.mongodb.net/auth_roles?retryWrites=true&w=majority')
+    app.listen(PORT, () => console.log(` Server has been started ${PORT}`))
+  } catch(e) {
+    console.log(e);
   }
 }
 
