0db431cd74cf831e6e43b5df00e04285fc83ec9d
Vera Krasnova <74105045+VerchkaFeya@users.noreply.github.com>
2022-08-20 09:58:53 +0300

    RS-Clone-33: create layout for movie page (#24)

---
+++ src/components/moviePage/MoviePageLayout/MoviePageLayout.tsx
@@ -0,0 +1,23 @@
+import { InterestingFacts } from "../InterestingFacts/InterestingFacts"
+import { MovieDescription } from "../MovieDescription/MovieDescription"
+import { MovieInfo } from "../MovieInfo/MovieInfo"
+import { UsersReviews } from "../UsersReviews/UsersReviews"
+import "./MoviePageLayout.scss"
+
+export const MoviePageLayout = () => {
+	return(
+		<main className="movie-page">
+			<MovieInfo></MovieInfo>
+			<div className="movie-page__content">
+				<div className="movie-page__column-1">
+					<MovieDescription></MovieDescription>
+					<InterestingFacts></InterestingFacts>
+					<UsersReviews></UsersReviews>
+				</div>
+				<div className="movie-page__column-2">
+					Этот блок под вопросом пусть пока будет тут
+				</div>
+			</div>
+		</main>
+	)
+}
