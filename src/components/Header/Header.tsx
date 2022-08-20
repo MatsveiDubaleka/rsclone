a12f387b1139ce95bef2b1fa3af93e9e2f40b488
Pavel Priladyshev <64291173+aibolit666@users.noreply.github.com>
2022-08-20 09:47:14 +0300

    RSClone-32: create layout for search movie card (#18)
    * feat: add dropdown list to header search
    
    * refactor: authorization navigation layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,52 @@
+import { useState, useEffect, SetStateAction } from "react";
+import axios from "axios";
+import { token } from "../../utils/token";
 import './Header.scss';
+import DropdownList from '../DropdownList/DropdownList';
 
 const Header = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const onChange = (e: { preventDefault: () => void; target: { value: SetStateAction<string>; }; }) => {
+    e.preventDefault();
+
+    setQuery(e.target.value);
+
+    axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${e.target.value}&page=1`, {
+			headers: {
+				'X-API-KEY': token
+			}
+		}).then(({ data }) => !data.errors ? setResults(data.films) : setResults([]));
+  };
+  
   return (
     <div className="header-wrapper">
       <header className="header">
         <div className="logo"><h1>KINOPOISK</h1></div>
         <div className="header-nav">
           <div className="search-wrap">
-            <input placeholder="Фильмы, сериалы, персоны" id="header-search" className="search" type="search" autoComplete="off" autoFocus />
+            <input placeholder="Фильмы, сериалы, персоны" 
+            id="header-search" className="search" type="text" 
+            autoComplete="off" autoFocus 
+            value={query}
+            onChange={onChange}
+            />
             <span className="search-settings" id="search-settings"></span>
             <span className="search-loop" id="search-loop"></span>
+            
+            {results.length > 0 && (
+            <ul className="results">
+              <DropdownList movies={results}  />
+            </ul>
+
+          )}
           </div>
           <div className="user-auth">
+          <button className="avatar-btn" hidden></button>
             <span className="flag" id="flag" hidden></span>
-            <button className="avatar-btn" hidden></button>
+            <a className="enter-link">Вход</a>
           </div>
-          <a className="enter-link">Вход</a>
         </div>
       </header>
     </div>
