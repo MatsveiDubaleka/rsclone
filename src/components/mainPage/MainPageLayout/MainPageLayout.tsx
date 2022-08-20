ae5ad94bc966508f2c94fdfaeda16416f41d834f
Pavel <pavel.priladyshev@gmail.com>
2022-08-20 21:20:17 +0300

    refactor: change back mainpage content

--- src/components/mainPage/MainPageLayout/MainPageLayout.tsx
+++ src/components/mainPage/MainPageLayout/MainPageLayout.tsx
@@ -1,8 +1,6 @@
-import { AdvancedSearchPage } from '../../AdvancedSearchPage/AdvancedSearchPage'
 import { MainPageMenu } from "../MainPageMenu/MainPageMenu"
-// import MovieCard from "../MovieCard/MovieCard"
-// import MovieCardsList from "../MovieCardsList/MovieCardsList"
-// import { YouInterestedList } from "../YouInterestedList/YouInterestedList"
+import MovieCardsList from "../MovieCardsList/MovieCardsList"
+import { YouInterestedList } from "../YouInterestedList/YouInterestedList"
 import "./MainPageLayout.scss"
 
 export const MainPageLayout = () => {
@@ -10,10 +8,9 @@ export const MainPageLayout = () => {
 		<main className="main-page">
 			<MainPageMenu></MainPageMenu>
 			<div className="main-page__content">
-				{/* <MovieCardsList title="Рекомендации"></MovieCardsList>
+				<MovieCardsList title="Рекомендации"></MovieCardsList>
 				<MovieCardsList title="Новинки"></MovieCardsList>
-				<YouInterestedList></YouInterestedList> */}
-				<AdvancedSearchPage />
+				<YouInterestedList></YouInterestedList>
 			</div>
 		</main>
 	)
