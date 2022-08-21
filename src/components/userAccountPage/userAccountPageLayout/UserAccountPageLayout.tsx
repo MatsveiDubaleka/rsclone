9df6a9390e05592d51a2491503acf399621bc685
Vera Krasnova <azirafail@mail.ru>
2022-08-21 13:48:03 +0300

    RSClone-36: create layout for user data

--- src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
+++ src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
@@ -1,9 +1,11 @@
+import { UserData } from '../UserData/UserData';
 import './UserAccountPageLayout.scss';
 
 export const UserAccountPageLayout = () => {
 	return(
 		<main className="user-account-page">
 			<div className="user-account-page__content">
+				<UserData></UserData>
 			</div>
 		</main>
 	)
