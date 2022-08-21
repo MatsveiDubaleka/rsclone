c71113db93ab3e4cb589b267c284aa09e8ac22f0
Vera Krasnova <azirafail@mail.ru>
2022-08-21 13:03:50 +0300

    RSClone-31: create layout for user account page

---
+++ src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
@@ -0,0 +1,10 @@
+import './UserAccountPageLayout.scss';
+
+export const UserAccountPageLayout = () => {
+	return(
+		<main className="user-account-page">
+			<div className="user-account-page__content">
+			</div>
+		</main>
+	)
+}
