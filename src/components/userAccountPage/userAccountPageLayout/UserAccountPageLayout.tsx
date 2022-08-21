889c574c3fba5064fd124312434e0c44b26299bb
Vera Krasnova <azirafail@mail.ru>
2022-08-21 14:24:31 +0300

    RSClone-31: add menu and user data components on page

--- src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
+++ src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
@@ -1,9 +1,13 @@
+import { UserAccountSideMenu } from '../UserAccountSideMenu/UserAccountSideMenu';
+import { UserData } from '../UserData/UserData';
 import './UserAccountPageLayout.scss';
 
 export const UserAccountPageLayout = () => {
 	return(
 		<main className="user-account-page">
+			<UserAccountSideMenu></UserAccountSideMenu>
 			<div className="user-account-page__content">
+				<UserData></UserData>
 			</div>
 		</main>
 	)
