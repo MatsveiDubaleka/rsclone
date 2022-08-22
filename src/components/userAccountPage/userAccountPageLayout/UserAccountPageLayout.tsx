00f8308e796eb37a58c865ef782847cb54a38a16
Vera Krasnova <74105045+VerchkaFeya@users.noreply.github.com>
2022-08-22 15:01:27 +0300

    RSClone-40: fix typo (#30)

--- src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
+++ src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
@@ -6,11 +6,11 @@ export const UserAccountPageLayout = () => {
 	return(
 		<main className="user-account-page">
 			<div className="user-account-page__content">
-			<UserAccountSideMenu></UserAccountSideMenu>
-			<div className="user-account-page__content">
-				<UserData></UserData>
+				<UserAccountSideMenu></UserAccountSideMenu>
+				<div className="user-account-page__content">
+					<UserData></UserData>
+				</div>
 			</div>
 		</main>
 	)
-}
 }
