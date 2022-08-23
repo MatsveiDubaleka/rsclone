f13dd69dbd3d30a04922818500fb4dba24b3bccb
Vera Krasnova <azirafail@mail.ru>
2022-08-23 22:46:18 +0300

    add butto styles

--- src/components/moviePage/WriteReviewForm/WriteReviewForm.tsx
+++ src/components/moviePage/WriteReviewForm/WriteReviewForm.tsx
@@ -5,31 +5,38 @@ export const WriteReviewForm = () => {
 
 const [inputValue, setInputValue] = useState("Заголовок");
 const [textAreaValue, setTextAreaValue] = useState("Текст рецензии");
+const [selectValue, setSelectValue] = useState("review-type");
 
 const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 	const target = e.target;
 	setInputValue((target as HTMLInputElement).value);
 }
 
-const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 	const target = e.target;
 	setTextAreaValue((target as HTMLTextAreaElement).value);
+}
+
+const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+	const target = e.target;
+	setSelectValue((target as HTMLSelectElement).value);
 }
 
 	return(
 		<div className="write-review">
 			<p className="write-review__username">Vasya</p>
 			<form className="review-form">
-				<select className="review-form__select">  
-					<option value='' selected disabled hidden>Тип рецензии</option>
-					<option>Положительная</option>
-					<option>Нейтральная</option>
-					<option>Отрицательная</option>
+				<select className="review-form__select" value={selectValue} onChange={handleSelectChange}>
+					<option value="review-type" disabled hidden>Тип рецензии</option>
+					<option value="positive">Положительная</option>
+					<option value="neutral">Нейтральная</option>
+					<option value="negative">Отрицательная</option>
 				</select>
 				<input className="review-form__input" value={inputValue} onChange={handleInputChange}></input>
-				<textarea className="review-form__textarea" value={textAreaValue} onChange={handleTextArea}></textarea>
+				<textarea className="review-form__textarea" value={textAreaValue} onChange={handleTextAreaChange}></textarea>
 				<button className='review-form__btn'>Опубликовать рецензию</button>
 			</form>
 		</div>
 	)
 }
+
