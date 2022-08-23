3e77d14d442f75a45e219473c554cca6d9597753
Vera Krasnova <azirafail@mail.ru>
2022-08-23 22:09:48 +0300

    add types and state

--- src/components/moviePage/WriteReviewForm/WriteReviewForm.tsx
+++ src/components/moviePage/WriteReviewForm/WriteReviewForm.tsx
@@ -1,6 +1,21 @@
+import React, { useState } from 'react';
 import './WriteReviewForm.scss';
 
 export const WriteReviewForm = () => {
+
+const [inputValue, setInputValue] = useState("Заголовок");
+const [textAreaValue, setTextAreaValue] = useState("Текст рецензии");
+
+const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const target = e.target;
+	setInputValue((target as HTMLInputElement).value);
+}
+
+const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+	const target = e.target;
+	setTextAreaValue((target as HTMLTextAreaElement).value);
+}
+
 	return(
 		<div className="write-review">
 			<p className="write-review__username">Vasya</p>
@@ -11,8 +26,9 @@ export const WriteReviewForm = () => {
 					<option>Нейтральная</option>
 					<option>Отрицательная</option>
 				</select>
-				<input className="review-form__input" value="Заголовок"></input>
-				<textarea className="review-form__textarea" value="Текст"></textarea>
+				<input className="review-form__input" value={inputValue} onChange={handleInputChange}></input>
+				<textarea className="review-form__textarea" value={textAreaValue} onChange={handleTextArea}></textarea>
+				<button className='review-form__btn'>Опубликовать рецензию</button>
 			</form>
 		</div>
 	)
