5a0f5716caf1112d1bd5e9f9b5ae31bb7ea4e1e8
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-23 18:27:35 +0300

    feat: add browser router to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
@@ -7,7 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 );
 
 reportWebVitals();
