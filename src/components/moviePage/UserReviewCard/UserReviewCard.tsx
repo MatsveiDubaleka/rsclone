1663be4a785fd3f4d024fa3b7021c5f7a455937c
Vera Krasnova <74105045+VerchkaFeya@users.noreply.github.com>
2022-08-24 21:50:32 +0300

    RsClone-43: reviews components logics (change config, and add api reaquests to review form) (#37)
    * RSClone-30: create basic html and scss
    
    * add types and state
    
    * add butto styles
    
    * add 'export' to config (api requests axios file)
    
    * RSClone-43: add api requests etc
    
    * change config with getData function
    
    * RSClone-43: add API requests for reviews

--- src/components/moviePage/UserReviewCard/UserReviewCard.tsx
+++ src/components/moviePage/UserReviewCard/UserReviewCard.tsx
@@ -1,18 +1,42 @@
+import { FC, useState } from "react";
 import "./UserReviewCard.scss";
 
-export const UserReviewCard = () => {
+
+export type Review = {
+	author: string,
+	date: string,
+	description: string,
+	kinopoiskId: number,
+	negativeRating: number,
+	positiveRating: number,
+	title: string,
+	type: string
+}
+
+type UserReviewCardProps = {
+	review: Review
+}
+
+export const UserReviewCard : FC<UserReviewCardProps> = ({ review }) => {
+
+	const [isShownFull, setIsShownFull] = useState(false);
+
+	const handleBtnClick = () => {
+		isShownFull ? setIsShownFull(false) : setIsShownFull(true);
+		console.log('IS SHOWN FULL', isShownFull);
+	} 
+
 	return(
-		<div className="user-review negative">
+		<div className={`user-review ${(review.type).toLocaleLowerCase()}`}>
 			<div className="user-review__header">
-				<p className="user-review__username">sapalakalusha</p>
-				<p className="user-review__date">12 августа 2011</p>
+				<p className="user-review__username">{review.author}</p>
+				<p className="user-review__date">{review.date}</p>
 			</div>
 			<div className="user-review__main">
-				<h4 className="user-review__title">Заголовок</h4>
-				<p className="user-review__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et ipsa eius soluta eligendi reprehenderit, asperiores aliquid blanditiis assumenda ratione odit, harum nemo eos temporibus veritatis hic laborum rem illum pariatur.</p>
-				<p className="user-review__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et ipsa eius soluta eligendi reprehenderit, asperiores aliquid blanditiis assumenda ratione odit, harum nemo eos temporibus veritatis hic laborum rem illum pariatur.</p>
+				<h4 className="user-review__title">{review.title}</h4>
+				<p className={`user-review__text ${isShownFull ? 'full-shown' : ''}`}>{review.description}</p>
 			</div>
-			<button className="user-review__show-btn">показать всю рецензию</button>
+			<button className="user-review__show-btn" onClick={handleBtnClick}>{isShownFull ? 'скрыть' : 'показать всю рецензию'}</button>
 		</div>
 	)
 }
--- src/components/moviePage/UsersReviews/UsersReviews.tsx
+++ src/components/moviePage/UsersReviews/UsersReviews.tsx
@@ -1,36 +1,78 @@
+import { useEffect, useState } from "react";
+import { getData } from "../../../utils/config";
 import { UserReviewCard } from "../UserReviewCard/UserReviewCard";
+import { WriteReviewForm } from "../WriteReviewForm/WriteReviewForm";
 import "./UsersReviews.scss";
 
-export const UsersReviews = () => { 
+type UsersReviewsProps = {
+	movieId: number;
+}
+
+type ReviewsData = {
+	items: any, // не смогла это затипизировать
+	total: number,
+	totalNegativeReviews: number,
+	totalNeutralReviews: number,
+	totalPages: number,
+	totalPositiveReviews: number,
+}
+
+export const UsersReviews = ({movieId} : UsersReviewsProps) => {
+
+	const [isReviewFormVisible, setVisibility] = useState<boolean>(false);
+	const [reviewsData, setReviewsData] = useState<ReviewsData>();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		getData(`v2.2/films/${movieId}/reviews?page=5&order=USER_POSITIVE_RATING_ASC`, setReviewsData, setIsLoading);
+	}, []);
+
+	const handleWriteReviewButton = () => {
+		setVisibility(isReviewFormVisible ? false : true);
+	}
+
+	const allReviews = reviewsData?.items;
+	console.log(allReviews);
+
+	//TODO
+	// в идеале надо сделать так, чтобы на странице была зелная, серая и красная цитаты
+	// оставила на потом потому что это сейчас не приоритет
+	// const firstPositiveReview = allReviews.find((review : Review) => review.type === "POSITIVE");
+	// const firstNeutralReview = allReviews.find((review : Review) => review.type === "NEUTRAL");
+	// const firstNegativeReview = allReviews.find(((review : Review) => review.type === "NEGATIVE"));
+
 	return(
 		<div className="users-reviews">
 			<h3 className="users-reviews__title">Рецензии зрителей</h3>
-			<button className="users-reviews__write-review-btn">Написать рецензию</button>
-			<div className="users-reviews__content">
-				<div className="users-reviews__reviews">
-					<UserReviewCard></UserReviewCard>
-					<UserReviewCard></UserReviewCard>
-					<UserReviewCard></UserReviewCard>
+			<button className="users-reviews__write-review-btn" onClick={handleWriteReviewButton}>Написать рецензию</button>
+			{ !isLoading && 
+				<div className="users-reviews__content">
+					<div className="users-reviews__reviews">
+							{ isReviewFormVisible ? <WriteReviewForm></WriteReviewForm> : <></>}
+							<UserReviewCard review={reviewsData?.items[0]}/>
+							<UserReviewCard review={reviewsData?.items[1]}/>
+							<UserReviewCard review={reviewsData?.items[2]}/>
+					</div>
+					<div className="users-reviews__info">
+							<div className="users-reviews__total active">
+								<p className="users-reviews__amount">{reviewsData?.total}</p>
+								<span>Всего</span>
+							</div>
+							<div className="users-reviews__positive">
+								<p className="users-reviews__amount positive">{reviewsData?.totalPositiveReviews}</p>
+								<span>Положительные</span>
+							</div>
+							<div className="users-reviews__neutral">
+								<p className="users-reviews__amount neutral">{reviewsData?.totalNeutralReviews}</p>
+								<span>Нейтральные</span>
+							</div>
+							<div className="users-reviews__negative">
+								<p className="users-reviews__amount negative">{reviewsData?.totalNegativeReviews}</p>
+								<span>Отрицательные</span>
+							</div>
+					</div>
 				</div>
-				<div className="users-reviews__info">
-					<div className="users-reviews__total active">
-						<p className="users-reviews__amount">45</p>
-						<span>Всего</span>
-					</div>
-					<div className="users-reviews__positive">
-						<p className="users-reviews__amount positive">13</p>
-						<span>Положительные</span>
-					</div>
-					<div className="users-reviews__neutral">
-						<p className="users-reviews__amount neutral">2</p>
-						<span>Нейтральный</span>
-					</div>
-					<div className="users-reviews__negative">
-						<p className="users-reviews__amount negative">17</p>
-						<span>Отрицательные</span>
-					</div>
-				</div>
-			</div>
+			}
 		</div>
 	)
 }
