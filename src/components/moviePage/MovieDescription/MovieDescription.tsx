f2f2f106ddac478cea75b36ef2227a8b6faf422d
Vera Krasnova <azirafail@mail.ru>
2022-08-27 13:32:45 +0300

    MovieDescription component: add types and props

--- src/components/moviePage/MovieDescription/MovieDescription.tsx
+++ src/components/moviePage/MovieDescription/MovieDescription.tsx
@@ -1,14 +1,15 @@
+import { FC } from "react";
 import "./MovieDescription.scss";
 
-export const MovieDescription = () => {
+type MovieDescriptionProps = {
+	description: string | undefined,
+}
+
+export const MovieDescription : FC<MovieDescriptionProps> = ({ description }) => {
 	return(
 		<div className="movie-description">
 			<h3 className="movie-description__title">Обзор</h3>
-			<p className="movie-description__text">Несколько месяцев назад в глухой деревушке бесследно пропал ребенок. 
-				Спустя время все почти забыли о происшедшем. Однажды в местную школу приезжает молодая учительница. 
-				Вместе с маленьким сыном она пытается начать новую жизнь, подальше сбежав от своего прошлого. 
-				Цепь загадочных событий указывает на то, что появление мальчика могло вновь пробудить древнее зло, 
-				затаившееся в окрестных лесах.</p>
+			<p className="movie-description__text">{description}</p>
 		</div>
 	)
 }
