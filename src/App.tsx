266b775023b8a72b73527e4e670196ec3549dd30
Vera Krasnova <azirafail@mail.ru>
2022-08-28 22:56:42 +0300

    add MoviePageLayout route to App comp

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { MainPageLayout } from './components/mainPage/MainPageLayout/MainPageLayout';
+import { MoviePageLayout } from './components/moviePage/MoviePageLayout/MoviePageLayout';
 import { SearchPageLayout } from './components/searchPage/SearchPageLayout/SearchPageLayout';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<MainPageLayout/>}/>
         <Route path='/searchPage' element={<SearchPageLayout />}/>
+        <Route path='movie/:id' element={<MoviePageLayout />}/>
       </Routes>
       <Footer /> 
     </div>
