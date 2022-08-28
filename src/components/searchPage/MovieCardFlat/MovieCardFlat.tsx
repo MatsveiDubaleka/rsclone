76115dde01e6c6e5b9a2e8a248496ebdf0413776
Vera Krasnova <azirafail@mail.ru>
2022-08-29 01:01:32 +0300

    fix colors and add conditional render

--- src/components/searchPage/MovieCardFlat/MovieCardFlat.tsx
+++ src/components/searchPage/MovieCardFlat/MovieCardFlat.tsx
@@ -7,28 +7,34 @@ import { NavLink } from 'react-router-dom';
 const MovieCardFlat : FC<IDropdownMovie> = ({num, nameRu, nameEn, posterUrlPreview, year, rating, filmId, filmLength, genres, countries}) => {
 	const[elem, setElem] = useState(false);
 
+	const infoMainArray = [nameEn, nameRu, year].filter(item => item !== undefined).join(', '); // эти переменные, чтобы не было висячих запятых в верстке
+	const countriesArray = countries.map(item => item.country).join(', ');
+	const genresArray = genres.map(item => item.genre).join(', ');
+
 	return (
 		<NavLink to={`/movie/${filmId}`}>
 		<li className="movie-card-flat" id={`${filmId}`} onClick={() => console.log(filmId)}>
 			<p className='movie-card-flat__number'>{num + 1}</p>
 			<div className="movie-card-flat__poster">
 				<div className="movie-card-flat__poster-image" style={{backgroundImage: `url(${posterUrlPreview})`}}>
-					<div className="movie-card-flat__poster-rating">{rating}</div>
+					<div className={`movie-card-flat__poster-rating ${rating < 5 ? 'negative' : rating > 7 ? 'positive' : ''}`}>
+						{String(rating) !== 'null' ? rating : '-'}
+					</div>
 				</div>
 			</div>
 			<div className='movie-card-flat__info'>
 				<h3 className='movie-card-flat__title-ru'>{nameRu}</h3>
 				<div className='movie-card-flat__info-main'>
-					<p className='movie-card-flat__title-en'>{nameEn},</p>
-					<p className='movie-card-flat__year'>{year},</p>
-					<p className='movie-card-flat__duration'><span>{filmLength}</span></p>
+					<p>{infoMainArray}</p>
 				</div>
 				<div className='movie-card-flat__info-sub'>
-					<p className='movie-card-flat__country'>Страна: {countries.map(x=>x.country + ', ')}</p>
+					<p className='movie-card-flat__country'>Страна: {countriesArray}</p>
 				</div>
-				<p className='movie-card-flat__genre'>Жанр: {genres.map(x=>x.genre + ', ')}</p>
+				<p className='movie-card-flat__genre'>Жанр: {genresArray}</p>
 			</div>
-			<p className='movie-card-flat__rating-imdb rating_good'>{rating}</p>
+				<p className={`movie-card-flat__rating ${rating < 5 ? 'negative' : rating > 7 ? 'positive' : ''}`}>
+					{String(rating) !== 'null' ? rating : '-'}
+				</p> 
 			<div className='movie-card-flat__buttons'>
 				<button className='movie-card-flat__to-watch-btn'><span className='flag'></span>Буду смотреть</button>
 				<button onClick={
