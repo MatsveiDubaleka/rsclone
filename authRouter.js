53ece2ebbce9a230967b584e5e6c1d0410606d9b
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-30 18:33:36 +0300

    feat: add routes to reviews and checks

--- authRouter.js
+++ authRouter.js
@@ -8,7 +8,15 @@ router.post('/registration', [
   check('username', 'Username cant be empty').notEmpty(),
   check('password', 'Password should consists of more than 4 letters').isLength({min: 4, max: 16})
 ], controller.registration)
+router.post('/reviews', [
+    check('author', 'Author cant be empty').notEmpty(),
+    check('title', 'Title cant be empty').notEmpty(),
+    check('text', 'Password should consists of more than 4 letters').isLength({min: 5, max: 250}),
+    check('type', 'Type should be neutral|positive|negative').notEmpty(),
+  ], controller.postReview)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
+router.get('/getReviews', controller.getReviews)
+
 
 module.exports = router;
