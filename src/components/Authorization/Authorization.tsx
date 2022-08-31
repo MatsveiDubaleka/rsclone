e0eb89c136c0205429c689a53207298ce294dfaa
Vera Krasnova <azirafail@mail.ru>
2022-08-31 23:56:05 +0300

    fix typo in autorization

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -8,12 +8,12 @@ const Authorization = () => {
         <div className="authorization-card">
 					<Link to={'/'}><button className="authorization-card__button-back"></button></Link>
           <div className="authorization-card__logo-img"></div>
-          <span className="authorization-card__tooltip">Войдите или зарегестрируйтесь</span>
+          <span className="authorization-card__tooltip">Войдите или зарегистрируйтесь</span>
           <form action="">
             <input name="login" type="text" placeholder="Логин и email" className="authorization-card__input login-auth" id="login-auth"/>
             <input name="password" type="text" placeholder="Пароль" className="authorization-card__input password-auth" id="password-auth"/>
           </form>
-          <button className="authorization-card__button-auth">Войти</button>
+          <Link to={`/my-account`}><button className="authorization-card__button-auth">Войти</button></Link>
           <button className="authorization-card__button-auth">Зарегистрироваться</button>
         </div>
       </div>
