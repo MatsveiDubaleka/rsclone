e58536e4f5945c2daa18c54537ef7f36d486e82e
Vera Krasnova <azirafail@mail.ru>
2022-08-31 21:21:29 +0300

    change review-form order

--- src/components/moviePage/UsersReviews/UsersReviews.tsx
+++ src/components/moviePage/UsersReviews/UsersReviews.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { createRef, FC, useEffect, useState } from "react";
 import { getData } from "../../../utils/config";
 import { MovieIdProps } from "../MoviePageLayout/MoviePageLayout";
 import { Review, UserReviewCard } from "../UserReviewCard/UserReviewCard";
@@ -17,27 +17,29 @@ type ReviewsData = {
 
 export const UsersReviews : FC<MovieIdProps> = ({ movieId }) => {
 
-	const [isReviewFormVisible, setVisibility] = useState<boolean>(false);
 	const [reviewsData, setReviewsData] = useState<ReviewsData>();
 
 	useEffect(() => {
 		getData(`v2.2/films/${movieId}/reviews?page=1&order=DATE_DESC`, setReviewsData);
 	}, [setReviewsData]);
 
-	const handleWriteReviewButton = () => {
-		setVisibility(isReviewFormVisible ? false : true);
+	const reviewFormRef = createRef();
+	const scrollToReviewForm = () => {
+		window.scrollTo(0, document.body.scrollHeight);
 	}
 
 	return(
 		<div className="users-reviews">
 			<h3 className="users-reviews__title">Рецензии зрителей</h3>
-			<button className="users-reviews__write-review-btn" onClick={handleWriteReviewButton}>Написать рецензию</button>
+			<button className="users-reviews__write-review-btn" onClick={scrollToReviewForm}>Написать рецензию</button>
 				<div className="users-reviews__content">
 					<div className="users-reviews__reviews">
-							{ isReviewFormVisible ? <WriteReviewForm></WriteReviewForm> : <></>}
 							{reviewsData?.items?.splice(0, 3).map((item : Review, index:number) => {
 								return <UserReviewCard key={`review-${index}`} review={item} />
 							})}
+							<div ref={reviewFormRef as React.RefObject<HTMLDivElement>}>
+								<WriteReviewForm></WriteReviewForm>
+							</div>
 					</div>
 					<div className="users-reviews__info">
 							<div className="users-reviews__total active">
