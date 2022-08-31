b475e514d961e51634bf32596871486f6bad4a31
Vera Krasnova <azirafail@mail.ru>
2022-08-31 11:54:40 +0300

    RSClone-56: fix movie lists on main page

--- src/components/mainPage/MainPageLayout/MainPageLayout.tsx
+++ src/components/mainPage/MainPageLayout/MainPageLayout.tsx
@@ -10,8 +10,8 @@ export const MainPageLayout = () => {
 			<MainPageMenu></MainPageMenu>
 			<div className="main-page__content">
 				<MovieVideo />
-				<MovieCardsList title="Рекомендации"></MovieCardsList>
-				<MovieCardsList title="Новинки"></MovieCardsList>
+				<MovieCardsList title="Рекомендации" url={"v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1"} listType="recommend"></MovieCardsList>
+				<MovieCardsList title="Новинки" url={"v2.2/films/premieres?year=2022&month=AUGUST"} listType="new"></MovieCardsList>
 				<YouInterestedList></YouInterestedList>
 			</div>
 		</main>
--- src/components/mainPage/MovieCardsList/MovieCardsList.tsx
+++ src/components/mainPage/MovieCardsList/MovieCardsList.tsx
@@ -1,31 +1,80 @@
 import { useState, useEffect, FC } from "react";
 import MovieCard from "../MovieCard/MovieCard";
-import { IMovieCardList } from "./types";
 import "./MovieCardsList.scss";
 import axios from "axios";
 import { token } from "../../../utils/token";
 
-const MovieCardsList: FC<IMovieCardList> = ({title}) => {
+type MovieCardList = {
+	title: string;
+	url: string,
+	listType: string
+}
 
-	const [movies, setMovies] = useState <any>([]);
+type MoviePopular = {
+	countries: [
+		{
+			country?: string | undefined
+		}
+	],
+	filmId: number,
+	filmLength: string,
+	nameRu: string,
+	nameEn: string,
+	posterUrl: string,
+	posterUrlPreview: string,
+	rating: string,
+	ratingChange: any, 
+	ratingVoteCount: number,
+	year: string
+}
+
+type MovieNew = {
+	countries: [
+		{
+			country?: string | undefined
+		}
+	],
+	duration: number,
+	genres: [
+		{
+			genre?: string | undefined
+		},
+	],
+	kinopoiskId: number,
+	nameRu: string,
+	nameEn: string,
+	posterUrl: string,
+	posterUrlPreview: string,
+	premiereRu: string,
+	year: string
+}
+
+type MovieArray = MoviePopular[] | MovieNew[];
+
+const MovieCardsList: FC<MovieCardList> = ({title, url, listType}) => {
+
+	const propType = listType === "new" ? 'items' : 'films';
+
+	const [movies, setMovies] = useState <MovieArray>([]);
 
 	useEffect(()=> {
-		axios.get('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1', {
+		axios.get(`https://kinopoiskapiunofficial.tech/api/${url}`, {
 			headers: {
 				'X-API-KEY': token
 			}
-		}).then(({ data }) => setMovies(data.films));
+		}).then(({ data }) => setMovies(data[propType]));
 
-	}, []);
+	}, [setMovies]);
 
-	movies.splice(6);
+	movies?.splice(6);
 
 	return (
 		<div className="movie-cards-list">
 			<h2 className="movie-cards-list__title">{title}</h2>
 			<div className="movie-cards-list__movies">
 				{movies?.map((movie: any) : JSX.Element => {
-					return <MovieCard kinopoiskId={movie.filmId} nameRu={movie.nameRu} posterUrlPreview={movie.posterUrlPreview} year={movie.year} rating={movie.rating} genre={movie.genres[0].genre} key={`movie-card-${movie.nameRu}`}></MovieCard>
+					console.log('id:', movie.kinopoiskId || movie.filmId);
+					return <MovieCard kinopoiskId={movie.kinopoiskId || movie.filmId} nameRu={movie.nameRu} posterUrlPreview={movie.posterUrlPreview} year={movie.year} rating={movie.rating} genre={movie.genres[0].genre} key={`movie-card-${movie.nameRu}`}></MovieCard>
 				})}
 			</div>
 		</div>
