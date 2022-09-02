8da7b9e6be9cc2b31a87a656df30058407294d4a
Vera Krasnova <azirafail@mail.ru>
2022-09-02 21:40:34 +0300

    add utils functions, change scss, add conditional render

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Authorization.scss';
+import { tryLogin, tryRegistration } from './utils';
 
 type AuthResponce = {
 	message?: string,
@@ -11,11 +12,15 @@ const Authorization = () => {
 
 const [login, setLogin] = useState('');
 const [password, setPassword] = useState('');
+const [isSignInVisible, setIsSignVisible] = useState(true);
 
 const navigate = useNavigate();
 
 // const userInfo = localStorage.getItem('user-info');
 
+const toggleSignInVisible = () => {
+	isSignInVisible ? setIsSignVisible(false) : setIsSignVisible(true);
+}
 
 
 const handleLoginInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -29,34 +34,30 @@ const handlePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
 }
 
 const handleLoginBtn = async () => {
-	const resp = await tryLogin();
-		if ((resp as AuthResponce).message) {
-			console.log("USER / PASSWORD INCORRECT")
+	const resp = await tryLogin(login, password);
+	if ((resp as AuthResponce).message) {
+		if ((resp as AuthResponce).message?.includes('password')) {
+			alert('некорректный пароль');
 		} else {
-			console.log("ACCESS OK")
-			localStorage.setItem("user-info", JSON.stringify(login));
-			navigate("/my-account");
+			alert('такого пользователя не существует');
 		}
+	} else {
+		localStorage.setItem("user-info", JSON.stringify(login));
+		navigate("/my-account");
+	}
 }
 
-const handleRegistrationBtn = () => {
-	console.log('registration clicked');
-}
-
-
-async function tryLogin() {
-	let resp = await fetch(`https://rskinopoisk.herokuapp.com/auth/login`, {
-		method: 'POST',
-		body: JSON.stringify({
-			username: login,
-      password: password,
-		}),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-	resp = await resp.json();
-	return(resp);
+const handleRegistrationBtn = async() => {
+	const resp = await tryRegistration(login, password);
+	if ((resp as AuthResponce).message?.includes('successfully')) {
+		localStorage.setItem("user-info", JSON.stringify(login));
+		console.log((resp as AuthResponce).message);
+		alert(`аккаунт ${login} успешно создан!`);
+		navigate("/my-account");
+	} else {
+		alert("К сожалению логин занят, попробуйте изменить его");
+	}
+		
 }
 
   return (
@@ -65,13 +66,27 @@ async function tryLogin() {
         <div className="authorization-card">
 					<Link to={'/'}><button className="authorization-card__button-back"></button></Link>
           <div className="authorization-card__logo-img"></div>
-          <span className="authorization-card__tooltip">Войдите или зарегистрируйтесь</span>
-          <form action="">
-            <input name="login" type="text" required placeholder="Логин" className="authorization-card__input login-auth" id="login-auth" value={login} onChange={handleLoginInput}/>
-            <input name="password" type="password" required placeholder="Пароль" className="authorization-card__input password-auth" id="password-auth" value={password} onChange={handlePasswordInput}/>
-          </form>
-          <button className="authorization-card__button-auth" onClick={handleLoginBtn} type='submit'>Войти</button>
-          <button className="authorization-card__button-auth" onClick={handleRegistrationBtn}>Зарегистрироваться</button>
+          <div className="authorization-card__tooltip">
+						<span className={`authorization-card__tooltip_sign-in ${isSignInVisible ? 'active' : ''}`} onClick={toggleSignInVisible}>Вход</span>
+						<span className={`authorization-card__tooltip_registration ${isSignInVisible ? '' : 'active'}`} onClick={toggleSignInVisible}>Регистрация</span>
+					</div>
+					{isSignInVisible ?
+					<>
+						<form action="" className='sign-in-form'>
+							<input name="login" type="text" required placeholder="Логин" className="authorization-card__input login-auth" id="login-auth" value={login} onChange={handleLoginInput}/>
+							<input name="password" type="password" required placeholder="Пароль" className="authorization-card__input password-auth" id="password-auth" value={password} onChange={handlePasswordInput}/>
+						</form>
+						<button className="authorization-card__button-auth" onClick={handleLoginBtn} type='submit'>Войти</button>
+					</>
+				: 
+					<>
+						<form action="" className='registration-form'>
+							<input name="login" type="text" required placeholder="Логин" className="authorization-card__input login-auth" id="login-auth" value={login} onChange={handleLoginInput}/>
+							<input name="password" type="password" required placeholder="Пароль" className="authorization-card__input password-auth" id="password-auth" value={password} onChange={handlePasswordInput}/>
+						</form>
+						<button className="authorization-card__button-auth" onClick={handleRegistrationBtn}>Зарегистрироваться</button>
+					</>
+				}
         </div>
       </div>
     </div>
---
+++ src/components/Authorization/utils.ts
@@ -0,0 +1,29 @@
+export async function tryLogin(login : string, password : string) {
+	let resp = await fetch(`https://rskinopoisk.herokuapp.com/auth/login`, {
+		method: 'POST',
+		body: JSON.stringify({
+			username: login,
+      password: password,
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	resp = await resp.json();
+	return(resp);
+}
+
+export async function tryRegistration(login : string, password : string) {
+	let resp = await fetch(`https://rskinopoisk.herokuapp.com/auth/registration`, {
+		method: 'POST',
+		body: JSON.stringify({
+			username: login,
+      password: password,
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	resp = await resp.json();
+	return(resp);
+}
