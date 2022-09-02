d7e0d76aa633cb637caa2336a521e501bab7d34e
Pavel Priladyshev <64291173+aibolit666@users.noreply.github.com>
2022-09-02 11:10:25 +0300

    Rs clone 60 add logic to advanced search page part 2 (#52)
    * fix: add close tag NavLink
    
    * fead: add logic
    
    * refactor: replace NavLink

--- src/components/searchPage/SearchPageLayout/SearchPageLayout.tsx
+++ src/components/searchPage/SearchPageLayout/SearchPageLayout.tsx
@@ -15,24 +15,30 @@ export const SearchPageLayout = () => {
 
 	const keyword: string | null = new URLSearchParams(location.search).get("keyword");
 	const year: string | null = new URLSearchParams(location.search).get("year");
-	const yearFrom: string | null = new URLSearchParams(location.search).get("yearFrom");
-	const yearTo: string | null = new URLSearchParams(location.search).get("yearTo");
-	const countries: string | null = new URLSearchParams(location.search).get("countries");
+	let yearFrom: string | null = new URLSearchParams(location.search).get("yearFrom")
+	let yearTo: string | null = new URLSearchParams(location.search).get("yearTo")
 	const rating: string | null = new URLSearchParams(location.search).get("rating");
-	const ratingFrom: string | null = new URLSearchParams(location.search).get("ratingFrom");
-	const ratingTo: string | null = new URLSearchParams(location.search).get("ratingTo");
+	let ratingFrom: string | null = new URLSearchParams(location.search).get("ratingFrom");
+	let ratingTo: string | null = new URLSearchParams(location.search).get("ratingTo");
+	const countries: string | null = new URLSearchParams(location.search).get("countries");
 	const genres: string | null = new URLSearchParams(location.search).get("genres");
+
+	year ? yearFrom = new URLSearchParams(location.search).get("year") : yearFrom = new URLSearchParams(location.search).get("yearFrom");
+	year ? yearTo = new URLSearchParams(location.search).get("year") : yearTo = new URLSearchParams(location.search).get("yearTo");
 	
-useEffect(()=> {
-	if(myParam){
-		axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${myParam}&page=1`, {
-			headers: {
-				'X-API-KEY': token
-			}
-		}).then(({ data }) => !data.errors ? setResults(data.films) : setResults([]));
-	} else {	
-		getDataItems(`v2.2/films?ratingFrom=${ratingFrom ? ratingFrom : "0"}&ratingTo=${ratingTo ? ratingTo : "10"}&yearFrom=${yearFrom ? yearFrom : "1900"}&yearTo=${yearTo ? yearTo : "2022"}&keyword=${keyword ? keyword : ""}&page=1`, setResults);
-	}
+	rating ? ratingFrom = new URLSearchParams(location.search).get("rating") : ratingFrom = new URLSearchParams(location.search).get("ratingFrom");
+	rating ? ratingTo = new URLSearchParams(location.search).get("rating") : ratingTo = new URLSearchParams(location.search).get("ratingTo");
+
+	useEffect(()=> {
+		if(myParam){
+			axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${myParam}&page=1`, {
+				headers: {
+					'X-API-KEY': token
+				}
+			}).then(({ data }) => !data.errors ? setResults(data.films) : setResults([]));
+		} else {	
+			getDataItems(`v2.2/films?countries=${Number(countries)>0 ? countries : ""}&genres=${Number(genres)>0 ? genres : ""}&order=RATING&type=ALL&ratingFrom=${ratingFrom ? ratingFrom : "0"}&ratingTo=${ratingTo ? ratingTo : "10"}&yearFrom=${yearFrom ? yearFrom : "1895"}&yearTo=${yearTo ? yearTo : "2022"}&keyword=${keyword ? keyword : ""}&page=1`, setResults);
+		}
 	},[setResults]);
 
 	return (
@@ -40,7 +46,7 @@ useEffect(()=> {
 			<MainPageMenu />
 			<div className="search-page__content">
 				<div className="search-page__content_header">
-					<div className="search-page__content_header_search-word">Поиск: <span><b>{myParam?.toUpperCase()}{keyword?.toUpperCase()}</b></span></div>
+					<div className="search-page__content_header_search-word">Поиск: <span><b>{myParam?.toUpperCase()}{keyword === 'undefined' ? "Ничего не найдено" : keyword?.toUpperCase()}</b></span></div>
 					<div className="search-page__content_header_total-results">Результаты: <span>{results.length}</span></div>
 				</div>
 					{(results.length>0) && <MovieCardFlatList movies={results} />}
