e2c39a702a0488c394c656719ac2ee8ed3fe9385
Vera Krasnova <azirafail@mail.ru>
2022-09-04 00:22:30 +0300

    create base for lists components and add it to routes

--- src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
+++ src/components/userAccountPage/userAccountPageLayout/UserAccountPageLayout.tsx
@@ -3,6 +3,8 @@ import { UserData } from '../UserData/UserData';
 import './UserAccountPageLayout.scss';
 import { Routes, Route} from 'react-router-dom';
 import { UserAccountReviews } from '../UserAccountReviews/UserAccountReviews';
+import { UserAccountToWatchList } from '../UserAccountToWatchList/UserAccountToWatchList';
+import { UserAccountWatchedList } from '../UserAccountWatchedList/UserAccountWatchedList';
 
 export const UserAccountPageLayout = () => {
 	return(
@@ -11,8 +13,8 @@ export const UserAccountPageLayout = () => {
 			<div className="user-account-page__content">
 				<Routes>
 					<Route path='/' element={<UserData/>}/>
-					<Route path='/to-watch' element={<UserData/>}/>
-					<Route path='/watched' element={<UserData/>}/>
+					<Route path='/to-watch' element={<UserAccountToWatchList/>}/>
+					<Route path='/watched' element={<UserAccountWatchedList/>}/>
 					<Route path='/my-reviews' element={<UserAccountReviews/>}/>
 				</Routes>
 			</div>
