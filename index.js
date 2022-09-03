e046c60fba781bd61ab78b4740474e48013d9ae2
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-09-03 18:55:33 +0300

    refactor: add other routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
 const express = require("express")
-const autoRouter = require('./authRouter')
+const authRouter = require('./routes/authRouter')
+const reviewsRouter = require('./routes/reviewsRouter')
+const trailerRouter = require('./routes/trailerRouter')
+
 const PORT = process.env.PORT || 5000
 const cors = require('cors');
 require('dotenv').config();
@@ -10,7 +13,9 @@ const app = express()
 app.use(cors()) // include before other routes
 
 app.use(express.json())
-app.use('/auth', autoRouter)
+app.use('/auth', authRouter)
+app.use('/reviews', reviewsRouter)
+app.use('/trailer', trailerRouter)
 
 const start = async () => {
   try{
