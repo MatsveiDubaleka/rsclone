5a03a46aa58ff2f963792ae54f85bfcc0b3344f7
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-09-03 18:55:17 +0300

    feat: add watchlist, favfilms, rating to user

--- models/User.js
+++ models/User.js
@@ -3,7 +3,13 @@ const {Schema, model} = require('mongoose')
 const User = new Schema({
   username: {type: String, unique: true, required: true},
   password: {type: String, required: true},
-  roles: [{type: String, ref: 'Role'}]
+  roles: [{type: String, ref: 'Role'}],
+  watchlist:[{kinopoiskId: {type: Number, default: 0}}],
+  favouriteFilms: [{kinopoiskId: {type: Number, default: 0}}],
+  ratingFilms: [{
+    kinopoiskId: {type: Number, default: 0},
+    ratingFilm: {type: Number, default: 0}
+  }]
 })
 
 module.exports = model('User', User)
