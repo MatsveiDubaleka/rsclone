cf992fbff021dcb9fb43d2d06753e3472d60c8b7
Vera Krasnova <azirafail@mail.ru>
2022-09-04 00:11:38 +0300

    add utils function for get user reviews from server

--- src/utils/utilsFunctions.ts
+++ src/utils/utilsFunctions.ts
@@ -6,3 +6,10 @@ export const getUsernameFromLocalStorage = () => {
 	const username = localStorage.getItem('username');
 	return username ? JSON.parse(username) : false;
 }
+
+export async function getUserReviewsByUser(username: string) {
+	const resp = await fetch(`https://rskinopoisk.herokuapp.com/reviews/getReviews`);
+	const reviews: any[] = await resp.json();
+	const userReviews : any[] = reviews.filter((review) => review.author === username);
+	return userReviews;
+}
