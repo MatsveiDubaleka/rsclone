2181c1f37bf7d315010ddbf3f929c33af1ca65d3
Pavel Priladyshev <64291173+aibolit666@users.noreply.github.com>
2022-09-04 14:18:24 +0300

    Rs clone 64 add trailers from backend (#57)
    * feat: add get last added trailer
    
    * feat: add last trailer response
    
    * fix: change path to trailer name
    
    * feat: add response trailer from bd

--- src/components/mainPage/MovieVideo/MovieVideo.tsx
+++ src/components/mainPage/MovieVideo/MovieVideo.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
 import { token } from '../../../utils/token';
 import ReactPlayer from 'react-player';
@@ -7,28 +7,50 @@ import { NavLink } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const movieTrailer = require('movie-trailer');
 
+type TrailersArray = {
+	nameEn: string;
+}
+
 const MovieVideo = () => {
 	const [trailer, setTrailer] = useState<any>([]);
 	const [videoURL, setVideoURL] = useState<string>("");
-	
+
 	const currentYear: string = new Date().getFullYear().toString();
 	const currentMonth: string = new Date().toLocaleString('en', {       
 		month: 'long'       
 	}).toUpperCase();
 
-	useEffect(() => {
-		axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=${currentYear}&month=${currentMonth}`, {
+	useEffect((): void => {
+		axios.get<TrailersArray[]>(`https://rskinopoisk.herokuapp.com/trailer/getTrailer`, {
 			headers: {
-				'X-API-KEY': token
+				'Content-Type': 'application/json'
 			}
-		}).then(({ data }) => {
-			const filteredData = data.items.filter((x: { nameEn: string }): boolean => x.nameEn.length > 0);
-			const len: number = Math.floor(Math.random() * filteredData.length);
-			setTrailer(filteredData[len])});
-	}, []);
+		}).then(({ data }: AxiosResponse<TrailersArray[]>): void => {
+				const filteredData: TrailersArray[] = data.filter((x: { nameEn: string }): boolean => x.nameEn.length > 0);
+				const lastElement: number = filteredData.length-1;
+				const filmName: string = filteredData[lastElement].nameEn;
+				
+				if(filmName !== undefined){
+					axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${filmName}&page=1`, {
+						headers: {
+							'X-API-KEY': token
+					}
+					}).then(({ data }) => setTrailer(data.films[0]));
+				} else {
+					axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=${currentYear}&month=${currentMonth}`, {
+						headers: {
+							'X-API-KEY': token
+						}
+					}).then(({ data }) => {
+						const filteredData = data.items.filter((x: { nameEn: string }): boolean => x.nameEn.length > 0);
+						const len: number = Math.floor(Math.random() * filteredData.length);
+						setTrailer(filteredData[len])})
+				}
+		})
+	}, [setTrailer]);
 
-	const nameEn: string = trailer?.nameEn; // get english film title
-
+	const nameEn: string | undefined = trailer.nameEn; // get english film title
+console.log(trailer)
 	movieTrailer(nameEn)
 		.then((response: string): void => {(response === null ? setVideoURL("https://youtu.be/5bqpcIX2VDQ") : setVideoURL(response))})
 		.catch((error: string): void => {
@@ -43,7 +65,7 @@ const MovieVideo = () => {
 				progressInterval={1000} width="940px" height="580px" />
 			</div>
 			<div className="movie-video__about">
-				<NavLink to={`/movie/${trailer.kinopoiskId}`}>
+				<NavLink to={`/movie/${trailer.filmId ? trailer.filmId : trailer.kinopoiskId}`}>
 					<div className="movie-video__title">{trailer.nameRu ? trailer.nameRu : trailer.nameEn}</div>
 				</NavLink>
 				<div className="movie-video__genre">{trailer.nameEn}</div>
