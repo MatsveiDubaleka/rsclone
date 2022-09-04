f5d5cbd5a8092ed67ae72856e0558636982f7146
Vera Krasnova <azirafail@mail.ru>
2022-09-04 20:42:49 +0300

    implemet interested-list component

--- src/components/mainPage/YouInterestedList/YouInterestedList.tsx
+++ src/components/mainPage/YouInterestedList/YouInterestedList.tsx
@@ -1,17 +1,43 @@
+import { useEffect, useState } from "react";
 import "./YouInterestedList.scss";
 
+type LocalStorageMovie = {
+  kinopoiskId: number,
+  posterUrlPreview: string
+}
+
+type LocalStorageList = LocalStorageMovie[];
+
 export const YouInterestedList = () => {
+
+  const [list, setList] = useState<LocalStorageList>([]);
+
+  useEffect(() => {
+    const result = localStorage.getItem('visitedMovies');
+    let parsedList : LocalStorageList ;
+    if (result) {
+      parsedList = JSON.parse(result);
+      setList(parsedList);
+    }
+  }, [])
+
 	return(
-		<div className="you-interested-list">
-		<h2 className="you-interested-list__title">Вы интересовались</h2>
-		<div className="you-interested-list__movies">
-			<div className="you-interested-list__movie"></div>
-			<div className="you-interested-list__movie"></div>
-			<div className="you-interested-list__movie"></div>
-			<div className="you-interested-list__movie"></div>
-			<div className="you-interested-list__movie"></div>
-			<div className="you-interested-list__movie"></div>
-		</div>
-	</div>
+    <>
+    {
+      list.length ? 
+      <>
+      <div className="you-interested-list">
+      <h2 className="you-interested-list__title">Вы интересовались</h2>
+      <div className="you-interested-list__movies">
+        {list?.reverse().slice(0, 7).map((item: LocalStorageMovie, index : number) => {
+          return <div id={`${item.kinopoiskId}`} key={`movie-${index}`} className="you-interested-list__movie" style={{ backgroundImage: `url(${item.posterUrlPreview})` }}></div>
+        })}
+      </div>
+    </div>
+      </>
+      : 
+      <></>
+    }
+</>
 	)
 }
