743654d7564ae0e296f37a1a2d8c995ab3ed16d1
Vera Krasnova <azirafail@mail.ru>
2022-09-04 20:14:00 +0300

    add function to utils for add movie to local storage

--- src/components/moviePage/MovieInfo/utils.ts
+++ src/components/moviePage/MovieInfo/utils.ts
@@ -1,4 +1,4 @@
-import { Budget, BudgetData, Cast, Distributions, DistributionsData, Person } from "./MovieInfo";
+import { Budget, BudgetData, Cast, Distributions, DistributionsData, Movie, Person } from "./MovieInfo";
 
 export const filterPersons = (persons: Cast, key : string) => {
 	if (key === "ACTOR") {
@@ -47,4 +47,38 @@ export const formatData = (date: string | undefined) => {
 		result.splice(1, 1, months[month]);
 		return result.join(' ');
 	}
+}
+
+type LocalStorageMovie = {
+  kinopoiskId: number,
+  posterUrlPreview: string
+}
+
+type LocalStorageList = LocalStorageMovie[];
+
+export const addMovieToLocalStorage = (movie: Movie) => {
+
+  const movieForLS = {
+    kinopoiskId: movie.kinopoiskId,
+    posterUrlPreview: movie.posterUrlPreview
+  }
+
+  if (localStorage.getItem('visitedMovies')) {
+    const visitedMovies = localStorage.getItem('visitedMovies');
+    if (visitedMovies) {
+      const arr = JSON.parse(visitedMovies);
+      console.log('parsed array', arr);
+      if (arr[arr.length - 1].kinopoiskId !== movie.kinopoiskId ) {
+        console.log('не повторяется фильм!')
+        arr.push(movieForLS);
+      } else {
+        console.log('повторяестя фильм!')
+      }
+      console.log('pushed array:', arr)
+      localStorage.setItem('visitedMovies',  JSON.stringify(arr));
+    }
+  } else {
+    localStorage.setItem('visitedMovies', JSON.stringify([movieForLS]));
+    console.log('set to local storage');
+  }
 }
