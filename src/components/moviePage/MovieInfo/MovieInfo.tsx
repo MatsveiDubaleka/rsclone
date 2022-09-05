3195f841cb5261298febacf4d937ecbfb6ee227f
Vera Krasnova <azirafail@mail.ru>
2022-09-05 14:48:57 +0300

    add loaders to movie info"

--- src/components/moviePage/MovieInfo/MovieInfo.tsx
+++ src/components/moviePage/MovieInfo/MovieInfo.tsx
@@ -118,102 +118,103 @@ export const MovieInfo: FC<MovieIdProps> = ({ movieId }) => {
   }
 
   return(
-    <>
+  <>
+    { movie?.nameRu ? 
+      <> 
       <div className="movie-info">
-      <div className="movie-info__column-1">
-        <div className="movie-info__poster" style={{ backgroundImage: `url(${movie?.posterUrl})`}}></div>
-        <div className="movie-info__trailer" style={{ backgroundImage: `url(${movie?.coverUrl})`}}></div>
-      </div>
-      <div className="movie-info__column-2">
-        {
-          movie?.nameRu ? 
+        <div className="movie-info__column-1">
+          <div className="movie-info__poster" style={{ backgroundImage: `url(${movie?.posterUrl})`}}></div>
+          <div className="movie-info__trailer" style={{ backgroundImage: `url(${movie?.coverUrl})`}}></div>
+        </div>
+        <div className="movie-info__column-2">
           <h2 className="movie-info__title">{ movie?.nameRu } {`(${movie?.year})`}</h2> :
-          <img src='https://samirkerzabi.com/wp-content/uploads/2021/02/1_9EBHIOzhE1XfMYoKz1JcsQ.gif' alt=''></img>
-        }
-        <div className="movie-info__buttons">
-          <button className="movie-info__to-watch-btn"></button>
-          <button onClick={(): void => elem ? setElem(undefined) : setElem(<MovieRatingSet />)} 
-            className="movie-info__to-rate-btn"></button>
-          {elem ? elem : null}
-        </div>
-        <h3 className="movie-info__about-title">О фильме</h3>
-        <div className="movie-info__main">
-          <p>Год производства</p>
-          <p className="movie-info__year">{ movie?.year }</p>
-          <p>Страна</p>
-          <p className="movie-info__countries">{movie?.countries?.map((item : Country)  => item.country).join(', ')}</p>
-          <p>Жанр</p>
-          <p className="movie-info__genres">{movie?.genres?.map((item : Genre) => item.genre).join(', ')}</p>
-          { movie?.slogan && 
-                <>
-                  <p>Слоган</p>
-                  <p className="movie-info__slogan">{movie?.slogan}</p>
-                </>
+          <div className="movie-info__buttons">
+            <button className="movie-info__to-watch-btn"></button>
+            <button onClick={(): void => elem ? setElem(undefined) : setElem(<MovieRatingSet />)} 
+              className="movie-info__to-rate-btn"></button>
+            {elem ? elem : null}
+          </div>
+          <h3 className="movie-info__about-title">О фильме</h3>
+          <div className="movie-info__main">
+            <p>Год производства</p>
+            <p className="movie-info__year">{ movie?.year }</p>
+            <p>Страна</p>
+            <p className="movie-info__countries">{movie?.countries?.map((item : Country)  => item.country).join(', ')}</p>
+            <p>Жанр</p>
+            <p className="movie-info__genres">{movie?.genres?.map((item : Genre) => item.genre).join(', ')}</p>
+            { movie?.slogan && 
+                  <>
+                    <p>Слоган</p>
+                    <p className="movie-info__slogan">{movie?.slogan}</p>
+                  </>
+              }
+            <p className="movie-info__director">Режиссёр</p>
+            <p>{ filterPersons(persons, "DIRECTOR")?.map((item : Person) => item.nameRu)}</p>
+            <p>Сценарий</p>
+            <p>{filterPersons(persons, "WRITER")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
+            <p>Продюсер</p>
+            <p>{filterPersons(persons, "PRODUCER")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
+            <p>Оператор</p>
+            <p>{filterPersons(persons, "OPERATOR")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
+            <p>Композитор</p>
+            <p>{filterPersons(persons, "COMPOSER")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
+            <p>Монтаж</p>
+            <p>{filterPersons(persons, "EDITOR")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
+            {budget?.items.length !== 0 && 
+              <>
+                <p>Бюджет</p>
+                <p className="movie-info__fees-world">{findBudget(budget, "BUDGET")}</p>
+              </>
             }
-          <p className="movie-info__director">Режиссёр</p>
-          <p>{ filterPersons(persons, "DIRECTOR")?.map((item : Person) => item.nameRu)}</p>
-          <p>Сценарий</p>
-          <p>{filterPersons(persons, "WRITER")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
-          <p>Продюсер</p>
-          <p>{filterPersons(persons, "PRODUCER")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
-          <p>Оператор</p>
-          <p>{filterPersons(persons, "OPERATOR")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
-          <p>Композитор</p>
-          <p>{filterPersons(persons, "COMPOSER")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
-          <p>Монтаж</p>
-          <p>{filterPersons(persons, "EDITOR")?.map((item : Person) => item.nameRu).join(', ') || '-'}</p>
-          {budget?.items.length !== 0 && 
+            {budgetRus && 
+              <>
+                <p>Сборы в России</p>
+                <p className="movie-info__fees-world">{budgetRus}</p>
+              </>
+            }
+            {findBudget(budget, "WORLD") &&
+              <>
+                <p>Сборы в мире</p>
+                <p className="movie-info__fees-world">{findBudget(budget, "WORLD")}</p>
+              </>
+            }
+            {findDWorldPremiere(distributions) &&
+              <>
+                <p>Премьера в мире</p>
+                <p className="movie-info__premiere-world">{formatData(findDWorldPremiere(distributions))}</p>
+              </>
+            }
+            {movie?.ratingAgeLimits && 
             <>
-              <p>Бюджет</p>
-              <p className="movie-info__fees-world">{findBudget(budget, "BUDGET")}</p>
-            </>
-          }
-          {budgetRus && 
-            <>
-              <p>Сборы в России</p>
-              <p className="movie-info__fees-world">{budgetRus}</p>
-            </>
-          }
-          {findBudget(budget, "WORLD") &&
-            <>
-              <p>Сборы в мире</p>
-              <p className="movie-info__fees-world">{findBudget(budget, "WORLD")}</p>
-            </>
-          }
-          {findDWorldPremiere(distributions) &&
-            <>
-              <p>Премьера в мире</p>
-              <p className="movie-info__premiere-world">{formatData(findDWorldPremiere(distributions))}</p>
-            </>
-          }
-          {movie?.ratingAgeLimits && 
-          <>
-            <p>Возраст</p>
-            <p>{formatAge(movie?.ratingAgeLimits)}</p>
-          </>}
-          { movie?.filmLength && 
-            <>
-              <p>Время</p>
-              <p className="movie-info__movie-length">{ `${movie?.filmLength} мин.`}</p>
-            </>
-          }
+              <p>Возраст</p>
+              <p>{formatAge(movie?.ratingAgeLimits)}</p>
+            </>}
+            { movie?.filmLength && 
+              <>
+                <p>Время</p>
+                <p className="movie-info__movie-length">{ `${movie?.filmLength} мин.`}</p>
+              </>
+            }
+          </div>
+        </div>
+        <div className="movie-info__column-3">
+          <p className={`movie-info__rating ${ratingClass}`}>{movie?.ratingKinopoisk || '-'}</p>
+          <p className="movie-info__ratings-total">{movie?.ratingKinopoiskVoteCount} оценки</p>
+          <p className="movie-info__reviews-total">{movie?.reviewsCount} рецензии</p>
+          <div className="movie-info__cast">
+            <h4 className="movie-info__cast-title">В главных ролях</h4>
+            {filterPersons(persons, "ACTOR")?.map((item : Person, index : number) => {
+                return <p className="movie-info__actor" key={`actor-${index}`}>{`${item.nameRu}`}</p>
+            })}
+            <p className="movie-info__actor">и другие...</p>
+          </div>
         </div>
       </div>
-      <div className="movie-info__column-3">
-        <p className={`movie-info__rating ${ratingClass}`}>{movie?.ratingKinopoisk || '-'}</p>
-        <p className="movie-info__ratings-total">{movie?.ratingKinopoiskVoteCount} оценки</p>
-        <p className="movie-info__reviews-total">{movie?.reviewsCount} рецензии</p>
-        <div className="movie-info__cast">
-          <h4 className="movie-info__cast-title">В главных ролях</h4>
-          {filterPersons(persons, "ACTOR")?.map((item : Person, index : number) => {
-              return <p className="movie-info__actor" key={`actor-${index}`}>{`${item.nameRu}`}</p>
-          })}
-          <p className="movie-info__actor">и другие...</p>
-        </div>
-      </div>
-    </div>
-    <MovieDescription description={movie?.description} />
-    <MovieRating movie={movie}/>
-    </>
+      <MovieDescription description={movie?.description} />
+      <MovieRating movie={movie}/>
+      </> :
+      <img src='https://samirkerzabi.com/wp-content/uploads/2021/02/1_9EBHIOzhE1XfMYoKz1JcsQ.gif' alt='' width={300}></img>
+    }
+  </>
   )
 }
